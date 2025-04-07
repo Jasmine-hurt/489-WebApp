b254f169c148141a440e1a0ce820c2a1d0606dd7
Nicholas Santos <santos_nick@outlook.com>
2025-04-07 12:12:41 -0700

    If user is not logged in, entering the viewAllDecks page brings the user back to the login page.

--- EnhancedQuizlet/routes/decks.js
+++ EnhancedQuizlet/routes/decks.js
@@ -3,8 +3,19 @@ var router = express.Router();
 const { Deck } = require('../db');
 
 router.get('/', async (req, res) => {
+    const userID = req.session.user?.id;
+
+    // user tries to view deck but is not logged in
+    if (!userID) {
+        req.flash('error', 'Please log in to view your decks.');
+        return res.redirect('/auth/login');
+    }
+
     try {
-        const decks = await Deck.findAll();
+        // Gets all decks that the current user is the owner of.
+        const decks = await Deck.findAll({
+            where: { userID }
+        });
         res.render('allDecksView', { decks });
     } catch (error) {
         console.error(error);
