88360fe399af8e8537639cbb1c404435d3b673c8
c-manangan <c.manangan@wsu.edu>
2025-04-08 15:04:53 -0700

    clicking home on navbar brings you home, also linked Games tab to view all available games

--- EnhancedQuizlet/app.js
+++ EnhancedQuizlet/app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 const decksRouter = require('./routes/decks');
 const flashcardsRouter = require('./routes/flashcards');
+const gamesRouter = require('./routes/games');
 
 var app = express();
 
@@ -48,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+app.use('/games', gamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
