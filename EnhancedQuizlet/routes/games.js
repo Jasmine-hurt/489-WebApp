9b1613b4b07ecdc699b613cf5f04ac3ddb3a5a73
Jasmine <Jasminehurt00@gmail.com>
2025-04-14 14:38:18 -0700

    created passage memory game

--- EnhancedQuizlet/routes/games.js
+++ EnhancedQuizlet/routes/games.js
@@ -55,4 +55,27 @@ router.get('/wordBank/play/:deckID', async (req, res) => {
   }
 });
 
+
+router.get('/memoryPassage/play/:deckID', async (req, res) => {
+  const userID = req.session.user?.id;
+  const deckID = req.params.deckID;
+
+  try {
+    const deck = await Deck.findByPk(deckID, {
+      include: [Flashcard]
+    });
+
+    if (!deck) return res.status(404).send("Deck not found.");
+    if (deck.userID !== userID) return res.status(403).send("Unauthorized.");
+
+    res.render('wordBank', {
+      flashcards: deck.Flashcards,
+      deckID: deck.deckID
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error loading Word Bank game.");
+  }
+});
+
 module.exports = router;
