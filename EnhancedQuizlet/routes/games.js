3a1f978aa6f6bd94aa1881610236ef67ca3bcf48
Nicholas Santos <santos_nick@outlook.com>
2025-04-14 15:11:05 -0700

    App can now navigate to matching game which uses deck flashcards.

--- EnhancedQuizlet/routes/games.js
+++ EnhancedQuizlet/routes/games.js
@@ -55,6 +55,27 @@ router.get('/wordBank/play/:deckID', async (req, res) => {
   }
 });
 
+router.get('/matching/play/:deckID', async (req, res) => {
+  const userID = req.session.user?.id;
+  const deckID = req.params.deckID;
+
+  try {
+    const deck = await Deck.findByPk(deckID, {
+      include: [Flashcard]
+    });
+
+    if (!deck) return res.status(404).send("Deck not found.");
+    if (deck.userID !== userID) return res.status(403).send("Unauthorized.");
+
+    res.render('matching', {
+      flashcards: deck.Flashcards,
+      deckID: deck.deckID
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error loading Word Bank game.");
+  }
+});
 
 router.get('/memoryPassage/play/:deckID', async (req, res) => {
   const userID = req.session.user?.id;
